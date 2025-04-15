b1df6386fbbdb2a75dca8d433b935b76bbe7536f
mzngu <eniyukuri97@gmail.com>
2025-04-15 11:21:02 +0200

    roles and permissions

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,12 @@ FROM jenkins/jenkins:lts
 USER jenkins
 RUN jenkins-plugin-cli --plugins \
     configuration-as-code \
-    job-dsl 
+    job-dsl \
+    cloudbees-folder \
+    credentials \
+    github \
+    instance-identity \
+    script-security \
+    structs \
+    role-strategy \
+    ws-cleanup \
--- launch_local_jenkins.sh
+++ launch_local_jenkins.sh
@@ -7,8 +7,8 @@ docker pull jenkins/jenkins:lts &&
   docker build -t dop-jenkins . &&
   docker run --rm -p 8080:8080 --name jenkins \
   -v "jenkins_home:/var/jenkins_home" \
-  -v "$(pwd)/configuration.yml:/var/jenkins_home/configuration.yml" \
+  -v "$(pwd)/my_marvin.yml:/var/jenkins_home/my_marvin.yml" \
   -v "$(pwd)/job_dsl.groovy:/var/jenkins_home/job_dsl.groovy" \
   -e 'JAVA_OPTS=-Djenkins.install.runSetupWizard=false' \
-  -e 'CASC_JENKINS_CONFIG=/var/jenkins_home/configuration.yml' \
+  -e 'CASC_JENKINS_CONFIG=/var/jenkins_home/my_marvin.yml' \
   dop-jenkins
