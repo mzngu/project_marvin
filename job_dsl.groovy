dd34f61be4abe3c1f34762a54bf3a2db660fd506
mzngu <eniyukuri97@gmail.com>
2025-04-16 22:21:41 +0200

    setup complete

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,22 +1,52 @@
-job("First job") {
-    steps {
-        shell('pwd')
-    }
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
 }
  
-job("Disk Space Check") {
-    steps {
-        shell('df')
-    }
-}
- 
-job("Daily Dose of Satisfaction") {
+job('Tools/clone-repository') {
     parameters {
-        stringParam('NAME', null, null)
+        stringParam('GIT_REPOSITORY_URL', "", 'Git URL of the repository to clone')
+    }
+    wrappers {
+        preBuildCleanup()
     }
     steps {
-        shell('echo "Hello dear $NAME!"')
-        shell('date')
-        shell('echo "Hello this is your DDos number $BUILD_NUMBER."')
+        shell('git clone $GIT_REPOSITORY_URL .')
+    }
+}
+ 
+job('Tools/SEED') {
+    parameters {
+        stringParam('GITHUB_NAME', null, 'GitHub repository owner/repo_name')
+        stringParam('DISPLAY_NAME', null, 'Display name for the job')
+    }
+    steps {
+        dsl {
+            text('''
+                job("$DISPLAY_NAME") {
+                    properties {
+                        githubProjectUrl("https://github.com/$GITHUB_NAME")
+                    }
+                    scm {
+                        git {
+                            remote {
+                                github("$GITHUB_NAME")
+                            }
+                        }
+                    }
+                    wrappers {
+                        preBuildCleanup()
+                    }
+                    triggers {
+                        scm('* * * * *')
+                    }
+                    steps {
+                        shell('make fclean')
+                        shell('make')
+                        shell('make tests_run')
+                        shell('make clean')
+                    }
+                }
+            ''')
+        }
     }
 }
